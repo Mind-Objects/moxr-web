da5ec8e18ee6017402a6e4dd5ce6bd82203689a4
Marten <gierth.marten@gmail.com>
2025-10-24 01:45:51 +0200

    Add 3D Explorer page with language switcher
    Introduces a new Explorer page for browsing 3D sketches, including MOXRGlbExplorer component, translations, and responsive styles. Adds a LanguageSwitch component for locale navigation, updates translations for the explorer, and refactors the homepage to use the new language switcher. Also includes new CSS for the explorer and updates global style variables.

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const urls = {
+  metaBeta: 'https://www.meta.com/s/4JKJaWuBi',
+  moxrYtTrailerID:"PMGjQxZqin4",
+  mindObjectsImprint: 'https://mind-objects.com/imprint/',
+  icuMedia: 'https://icu-media.xyz/',
+};
